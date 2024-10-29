78743d811ab785dc6ae533bb1cba16acd822f2d1
b0szazal <b0szazal@vasvari.org>
2024-10-29 11:53:26 +0100

    formvalidation maybe done

--- public/js/formValidation.js
+++ public/js/formValidation.js
@@ -1,1 +1,65 @@
-/*NYI*/
+/*this code is ass*/
+$(()=>{
+    let subjectGood=false;
+    let dayGood=false;
+    let fromTimeGood=false;
+    $("#subject").on("input", function(){
+        let subject = $(this).val();
+        if(subject.trim().length > 0){
+            subjectGood=true;
+        }
+        else{
+            subjectGood=false;
+        }
+        TryEnableSubmit();
+    })
+    $("#day").on("change", function(){
+        let day=$(this).find(":selected").val();
+        if(day=="-"){
+            dayGood=false;
+        }
+        else{
+            dayGood=true;
+        }
+        EnableSubmit();
+    })
+    $("#fromTime").on("input", function(){
+        try{
+            let fromTime = Number($(this).val());
+            if(fromTime>=0 && fromTime<24){
+                fromTimeGood=true;
+            }
+            else{
+                fromTimeGood=false;
+            }
+        }
+        catch{
+            fromTimeGood=false;
+        }
+        TryEnableSubmit();
+    })
+    $("#lengthTime").on("input", function(){
+        try{
+            let lengthTime = Number($(this).val());
+            if(fromTime>=0 && fromTime<169){
+                fromTimeGood=true;
+            }
+            else{
+                fromTimeGood=false;
+            }
+        }
+        catch{
+            fromTimeGood=false;
+        }
+        TryEnableSubmit();
+    })
+})
+
+function TryEnableSubmit(){
+    if(subjectGood && dayGood){
+        $("#submitBtn").prop("disabled", false);
+    }
+    else{
+        $("#submitBtn").prop("disabled", true);
+    }
+}
--- routes/addToTimeTable.js
+++ routes/addToTimeTable.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
     res.render('addToTimeTable', { title: 'Timetable Project' });;
 });
 
+router.post('/', (req, res) => {
+    // NYI
+    console.log(req.body);
+    res.redirect('/');
+})
+
 export {router as addToTimeTableRouter};
